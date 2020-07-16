b5c6a2b80e559044b6f54f293052b2e7ae9c3d7b
zubtsov.r <zubtsov.r@gmail.com>
2020-07-16 21:36:26 +0300

    tmp

--- src/test/scala/samples/AppSuite.scala
+++ src/test/scala/samples/AppSuite.scala
@@ -5,31 +5,39 @@ import org.scalatest.FunSuite
 
 class AppSuite extends FunSuite {
   val spark = SparkSession.builder()
-      .master("local[*]")
-      .getOrCreate()
+    .master("local[*]")
+    .getOrCreate()
 
   import spark.implicits._
 
-  def generateColumnCombinations(cols: Seq[String], combSize: Int) = {
-
+  def generateColumnCombinations(cols: Seq[String], combSize: Int): Seq[Seq[String]] = {
+    if (combSize == 1)
+      cols.map(Seq(_))
+    else
+      ???
   }
 
-  def potentialPrimaryKeysStatistics(df: DataFrame, maxPKSize: Int) = {
+  def identifyPotentialPrimaryKeys(df: DataFrame, maxPKSize: Int) = {
+    import org.apache.spark.sql.functions._
+
     val cols = df.columns
     if (maxPKSize > cols.size) throw new Exception("The size of composite primary key can't be greater than the number of columns!")
 
-    for (currentPKSize <- 1 to maxPKSize) {
-      ???
-    }
+    val combs = (1 to maxPKSize).map(currentPKSize => {
+      generateColumnCombinations(cols, currentPKSize)
+    }).reduce(_ ++ _)
+
+    df.groupBy().agg(count(lit(1)).as("rows"),
+      combs.map(c => countDistinct(lit(1), c.map(col): _*).as(c.mkString(","))): _*)
   }
 
   test("Identifying primary key") {
     val testData = Seq(
       (1, "str1", 1.1),
       (2, "str2", 2.2),
-      (3, "str3", 3.3)
+      (1, "str3", 3.3)
     ).toDF("integer_col", "string_col", "double_col")
 
-    testData.show()
+    identifyPotentialPrimaryKeys(testData, 1).show()
   }
 }
